8b74f6786b0e2271f0f0541b2b5ed8582c924943
baonoob1011 <trandinhbao222@gmail.com>
2026-07-13 12:04:59 +0700

    Initial commit

---
+++ src/main/java/com/livingdocs/livingdocs_platform/LivingdocsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.livingdocs.livingdocs_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingdocsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingdocsPlatformApplication.class, args);
+	}
+
+}
